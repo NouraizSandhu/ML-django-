bc2cf50d1174eb9a48be3ee0b60f3bfdc4d9273b
Nouraiz Sandhu <147933867+NouraizSandhu@users.noreply.github.com>
2023-11-30 17:02:20 +0500

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for SONAR_Rock_VS_Mine_Prediction project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SONAR_Rock_VS_Mine_Prediction.settings')
+
+application = get_asgi_application()
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for SONAR_Rock_VS_Mine_Prediction project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SONAR_Rock_VS_Mine_Prediction.settings')
+
+application = get_wsgi_application()
